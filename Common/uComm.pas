d51b0f94bb9cfc8f214e9d999cb4192a1513c5d1
EvanChen <chhlin2006@126.com>
2018-09-09 22:01:09 +0800

    增加基础数据加载方法

--- Common/uComm.pas
+++ Common/uComm.pas
@@ -8,12 +8,18 @@
 unit uComm;
 
 interface
-uses Classes,DB,cxGridCustomView, cxGridCustomTableView,cxGridDBTableView;
+uses Classes,DB,DBClient,ADODB,cxGridCustomView, cxGridCustomTableView,cxGridDBTableView,uDM,Forms,System.SysUtils;
   //设置字段内容显示位置
   procedure SetDataSetFieldPosition(ADataSet: TDataSet; AAlignment: TAlignment = taLeftJustify);
   //创建列字段
   procedure CreateGridColumns(AcxgDBTableView: TcxGridDBTableView;AFieldName,ACaption: string;AWidth: Integer;AMergeFlag: Boolean);
+  //建立基础数据缓存
+  procedure CreateBaseDataCache;
+  //重新某个基础数据缓存
+  procedure CreateDataDataCacheByDateType(const ADataType: string);
 
+var
+  cdsOprCutWound: TClientDataSet; //手术切口数据集
 implementation
 
 procedure SetDataSetFieldPosition(ADataSet: TDataSet; AAlignment: TAlignment = taLeftJustify);
@@ -34,6 +40,80 @@ begin
   cxcol.Width := AWidth;
   if AMergeFlag then
     cxcol.Options.CellMerging := True;
+end;
+
+procedure CreateBaseDataCache;
+var
+  qry: TADOQuery;
+begin
+  qry := TADOQuery.Create(nil);
+  try
+    qry.Connection := DM.conHAIMIS;
+    qry.LockType := ltReadOnly;
+    qry.SQL.Text := 'SELECT * FROM V_COMMON_TABLE_DATA ORDER BY TABLE_HEAD_ID,DISPLAY_ORDER';
+    qry.Open;
+    //加载手术切口等级
+    qry.Filtered := False;
+    qry.Filter := 'TABLE_HEAD_CODE=''OPER_CUT_WOUND''';
+    qry.Filtered := True;
+    if cdsOprCutWound = nil then
+      cdsOprCutWound := TClientDataSet.Create(Application);
+    with cdsOprCutWound do
+    begin
+      FieldDefs.Clear;
+      FieldDefs.Add('CODE',qry.FieldByName('LINE_CODE').DataType,qry.FieldByName('LINE_CODE').Size);
+      FieldDefs.Add('NAME',qry.FieldByName('LINE_NAME').DataType,qry.FieldByName('LINE_NAME').Size);
+      CreateDataSet;
+      Open;
+      qry.First;
+      while not qry.Eof do
+      begin
+        Append;
+        FieldByName('CODE').AsString := qry.FieldByName('LINE_CODE').AsString;
+        FieldByName('NAME').AsString := qry.FieldByName('LINE_NAME').AsString;
+        Post;
+        qry.Next;
+      end;
+    end;
+  finally
+    qry.Free;
+  end;
+end;
+
+procedure CreateDataDataCacheByDateType(const ADataType: string);
+var
+  qry: TADOQuery;
+  vSql: string;
+  cds: TClientDataSet;
+begin
+  qry := TADOQuery.Create(nil);
+  try
+    qry.Connection := DM.conHAIMIS;
+    qry.LockType := ltReadOnly;
+    if ADataType = 'OPER_CUT_WOUND' then
+    begin
+      vSql := 'SELECT * FROM V_COMMON_TABLE_DATA WHERE TABLE_HEAD_CODE = '+ QuotedStr(ADataType) +' ORDER BY TABLE_HEAD_ID,DISPLAY_ORDER';
+      cds := cdsOprCutWound;
+    end;
+    cds.EmptyDataSet;
+    qry.SQL.Text := vSql;
+    qry.Open;
+    qry.First;
+    if ADataType = 'OPER_CUT_WOUND' then
+    begin
+      while not qry.Eof do
+      begin
+        cds.Append;
+        cds.FieldByName('CODE').AsString := qry.FieldByName('LINE_CODE').AsString;
+        cds.FieldByName('NAME').AsString := qry.FieldByName('LINE_NAME').AsString;
+        cds.Post;
+        qry.Next;
+      end;
+    end;
+  finally
+    qry.Free;
+  end;
+
 end;
 
 end.
